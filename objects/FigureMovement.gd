3601f99160b8a9f8cd6e03e1a6a978c01a81b61d
Dominik Burgdörfer <db@burgdoerfer.com>
2020-04-18 17:35:01 +0200

    Add proper movement behaviour

--- objects/FigureMovement.gd
+++ objects/FigureMovement.gd
@@ -1,26 +1,18 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
-const GRAVITY = 9.81
+const GRAVITY = 200.0
+const WALK_SPEED = 100
 
 var velocity = Vector2()
 
-func get_input():
-	velocity
-	if Input.is_action_pressed('right'):
-		velocity.x += 1
-	if Input.is_action_pressed('left'):
-		velocity.x -= 1	
-	velocity = velocity.normalized() * speed
-
-func _physics_process(delta):	
-	
+func _physics_process(delta):
 	velocity.y += delta * GRAVITY
-	
-	if Input.is_action_pressed('right'):
-		velocity.x += 1
-	if Input.is_action_pressed('left'):
-		velocity.x -= 1	
-	
-	
-	velocity = move_and_slide(velocity)
+
+	if Input.is_action_pressed("left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("right"):
+		velocity.x =  WALK_SPEED
+	else:
+		velocity.x = 0
+
+	move_and_slide(velocity)
