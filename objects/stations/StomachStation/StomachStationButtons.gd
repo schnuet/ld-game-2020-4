629d3b49ae234282f1aa61eb01ccc1d126e14c77
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2020-04-20 01:34:56 +0200

    add new sprites for rooms

---
+++ objects/stations/StomachStation/StomachStationButtons.gd
@@ -0,0 +1,8 @@
+extends "res://objects/stations/stationButtons/StationButtons.gd"
+
+
+func _process(delta):
+	if (player == null): return;
+	var can_take_protein = player.get_node("ResourceStore").can_add_protein;
+	
+	$TakeProteinStationButton.visible = can_take_protein;
