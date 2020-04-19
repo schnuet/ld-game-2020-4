5fa5bcec790fbb99e815e6e97bb90de86b38d26f
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2020-04-19 11:45:30 +0200

    add signal that notify for changes on ResourceStore object

--- objects/resources/ResourceStore.gd
+++ objects/resources/ResourceStore.gd
@@ -5,16 +5,20 @@ var protein:int = 0;
 export var max_energy:int = 1;
 export var max_protein:int = 1;
 
+signal protein_amount_changed;
+signal energy_amount_changed;
 
 # add energy and protein
 
-func add_protein(x:int):
-	protein = min(protein + x, max_protein);
-	return protein;
-
 func add_energy(x:int):
 	energy = min(energy + x, max_energy);
+	emit_signal("energy_amount_changed", energy);
 	return energy;
+
+func add_protein(x:int):
+	protein = min(protein + x, max_protein);
+	emit_signal("protein_amount_changed", protein);
+	return protein;
 
 
 # get energy and protein
@@ -22,9 +26,11 @@ func add_energy(x:int):
 func take_energy(x:int = 1):
 	if (x > energy): return 0;
 	energy -= x;
+	emit_signal("energy_amount_changed", energy);
 	return x;
 
 func take_protein(x:int = 1):
 	if (x > protein): return 0;
 	protein -= x;
+	emit_signal("protein_amount_changed", protein);
 	return x;
