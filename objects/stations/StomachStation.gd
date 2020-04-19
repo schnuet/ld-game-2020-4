b194074395c652d50525cf30f91c853257dc7021
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2020-04-19 08:53:52 +0200

    add base action for stomach station

--- objects/stations/StomachStation.gd
+++ objects/stations/StomachStation.gd
@@ -1,10 +1,12 @@
 extends "res://objects/stations/Station.gd"
 
+var protein_created_on_action = 1;
+var energy_created_on_action = 1;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func perform_action():
+	add_protein(protein_created_on_action);
+	add_energy(energy_created_on_action);
