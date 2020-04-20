46162ce2d412d1f335102c2c6c47a64e6c42cf8a
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2020-04-20 12:18:20 +0200

    add a limit to the amount of created minions

--- objects/stations/KidneyStation.gd
+++ objects/stations/KidneyStation.gd
@@ -1,8 +1,30 @@
-extends "res://objects/stations/Station.gd"
-
-signal minion_creation_requested
+extends "res://objects/stations/Station.gd";
 
 var Minion = preload("res://objects/Minions/Minion.tscn");
 
-func perform_action():	
-	emit_signal("minion_creation_requested")
+signal minion_creation_requested
+
+export var max_creatable_minions = 1;
+
+
+func perform_action():
+	create_minion();
+	
+func update():
+	.update();
+	max_creatable_minions = floor(max_creatable_minions * 1.25);
+
+
+func get_can_create_minion():
+	return get_all_minions().size() < max_creatable_minions;
+
+func get_all_minions():
+	return get_tree().get_nodes_in_group("Minion");
+
+
+func create_minion():
+	if (get_all_minions().size() >= max_creatable_minions):
+		return false;
+	
+	emit_signal("minion_creation_requested");
+	print(get_all_minions().size());
