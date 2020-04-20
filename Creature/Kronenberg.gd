0ecef386ccdc620bc152367a356c05ab99e5093b
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2020-04-20 13:30:49 +0200

    create kronenberg background object (with animations)

---
+++ Creature/Kronenberg.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var chew_passes = 0;
+var max_chew_passes = 3;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_AnimatedSprite_animation_finished():
+	match $AnimatedSprite.animation:
+		"fressen":
+			$AnimatedSprite.animation = "kauen";
+			chew_passes = 0;
+		"kauen":
+			chew_passes += 1;
+			if (chew_passes >= max_chew_passes):
+				$AnimatedSprite.animation = "schlucken";
+		"schlucken":
+			$AnimatedSprite.animation = "idle";
+		"idle":
+			$AnimatedSprite.animation = "fressen";
+			
