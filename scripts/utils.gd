1c8bdb196b918162f2f18ac5339053196d45cd54
Ntimi Schnütgen <ntimi.schnuetgen@gmx.de>
2020-04-21 02:32:49 +0200

    count seconds alive

--- scripts/utils.gd
+++ scripts/utils.gd
@@ -3,7 +3,6 @@ extends Node
 # put helper functions in here
 
 var seconds_alive = 0;
-var timer;
 
 static func filter(filter_function: FuncRef, candidate_array: Array)->Array:
     var filtered_array := []
@@ -14,16 +13,8 @@ static func filter(filter_function: FuncRef, candidate_array: Array)->Array:
 
     return filtered_array
 
-func start_life_count():
-	seconds_alive = 0;
-	timer = Timer.new();
-	timer.start(1);
-	timer.connect("timeout", self, "count_up");
-	
-func stop_life_count():
-	timer.stop();
-
 func count_up():
+	print("alive for " + str(seconds_alive) + " seconds");
 	seconds_alive += 1;
 
 func get_count():
